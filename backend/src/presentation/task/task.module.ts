9ff80628c774dfcdd6bea7b8e7494b2a060d6326
Dedi Setiawan <dedi.setiawan@primecash.co.id>
2024-08-29 17:20:19 +0700

    add notification service to provider task module

--- backend/src/presentation/task/task.module.ts
+++ backend/src/presentation/task/task.module.ts
@@ -5,11 +5,12 @@ import { TaskController } from './task.controller';
 import { TaskService } from 'src/application/services/task.service';
 import { TaskRepository } from 'src/infrastructure/repositories/task.repository';
 import { AuthModule } from '../auth/auth.module';
+import { NotificationService } from 'src/application/services/notification.service';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Task]), AuthModule],
   controllers: [TaskController],
-  providers: [TaskService, TaskRepository],
+  providers: [TaskService, TaskRepository, NotificationService],
   exports: [TaskService],
 })
 export class TaskModule {}
