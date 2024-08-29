1f90df221131a95e826173c939a516a1eefdb35a
setiawanDedi05 <dssetiawan05@gmail.com>
2024-08-30 00:21:42 +0700

    add helmet library and implement them, and allow cors origin

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,10 +1,29 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as cookieParser from 'cookie-parser';
-
+import helmet from 'helmet';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  
+  app.use(helmet());
+
+  const allowedOrigins = [
+    'https://8e39-114-124-246-116.ngrok-free.app',
+    'http://localhost:3000'
+  ]
+
+  app.enableCors({
+    origin: (origin, callback) => {
+      if(allowedOrigins.includes(origin) || !origin){
+        callback(null, true)
+      }else{
+        callback(new Error('Not Allowed by Cors'))
+      }
+    },
+    methods: 'GET, POST, PUT, PATCH, HEAD, DELETE',
+    credentials: true
+  })
   app.use(cookieParser());
-  await app.listen(3000);
+  await app.listen(3001);
 }
 bootstrap();
