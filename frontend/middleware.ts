98f0be8de31c3c0018345c63397e1b94c1ebc263
setiawanDedi05 <dssetiawan05@gmail.com>
2024-09-01 21:11:31 +0700

    hange matcher

--- frontend/middleware.ts
+++ frontend/middleware.ts
@@ -10,5 +10,5 @@ export function middleware(req: NextRequest) {
 }
 
 export const config = {
-  matcher: ["/dashboard/:path*", "/project/:path*", "/task/:path*"],
+  matcher: ["/protected/:path*"],
 };
