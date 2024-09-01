ff288525f6438c69e471987c5f74991604376eb8
setiawanDedi05 <dssetiawan05@gmail.com>
2024-09-01 21:11:22 +0700

    add suspense in user-nav

--- frontend/components/ui/user-nav.tsx
+++ frontend/components/ui/user-nav.tsx
@@ -12,6 +12,7 @@ import { authService } from "@/services/auth/authService";
 import { toast } from "./use-toast";
 import { useAuthStore } from "@/store/auth-store";
 import { useRouter } from "next/navigation";
+import { Suspense } from "react";
 
 export function UserNav() {
   const { user, removeSeason } = useAuthStore();
@@ -42,34 +43,36 @@ export function UserNav() {
       });
     }
   }
-  if(user === null) {
-    return <></>
-  }
+
   return (
-    <DropdownMenu>
-      <DropdownMenuTrigger asChild>
-        <Button variant="ghost" className="relative h-8 w-8 rounded-full">
-          <Avatar className="h-8 w-8">
-            <AvatarImage
-              src="/avatars/01.png"
-              alt={`avatar-${user!.username}`}
-            />
-            <AvatarFallback>{user!.username.slice(0, 2)}</AvatarFallback>
-          </Avatar>
-        </Button>
-      </DropdownMenuTrigger>
-      <DropdownMenuContent className="w-56" align="end" forceMount>
-        <DropdownMenuLabel className="font-normal">
-          <div className="flex flex-col space-y-1">
-            <p className="text-sm font-medium leading-none">{user!.username}</p>
-            <p className="text-xs leading-none text-muted-foreground">
-              {user!.email}
-            </p>
-          </div>
-        </DropdownMenuLabel>
-        <DropdownMenuSeparator />
-        <DropdownMenuItem onClick={onClick}>Log out</DropdownMenuItem>
-      </DropdownMenuContent>
-    </DropdownMenu>
+    <Suspense fallback={<p>Loading...</p>}>
+      <DropdownMenu>
+        <DropdownMenuTrigger asChild>
+          <Button variant="ghost" className="relative h-8 w-8 rounded-full">
+            <Avatar className="h-8 w-8">
+              <AvatarImage
+                src={`/avatars/${user?.username}.png`}
+                alt={`avatar-${user?.username}`}
+              />
+              <AvatarFallback>{user?.username?.slice(0, 2)}</AvatarFallback>
+            </Avatar>
+          </Button>
+        </DropdownMenuTrigger>
+        <DropdownMenuContent className="w-56" align="end" forceMount>
+          <DropdownMenuLabel className="font-normal">
+            <div className="flex flex-col space-y-1">
+              <p className="text-sm font-medium leading-none">
+                {user?.username}
+              </p>
+              <p className="text-xs leading-none text-muted-foreground">
+                {user?.email}
+              </p>
+            </div>
+          </DropdownMenuLabel>
+          <DropdownMenuSeparator />
+          <DropdownMenuItem onClick={onClick}>Log out</DropdownMenuItem>
+        </DropdownMenuContent>
+      </DropdownMenu>
+    </Suspense>
   );
 }
