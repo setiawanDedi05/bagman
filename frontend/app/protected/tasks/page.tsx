ed65861a45042947a8899937288299b420b538cf
setiawanDedi05 <dssetiawan05@gmail.com>
2024-09-02 08:54:09 +0700

    create task page content

--- frontend/app/protected/tasks/page.tsx
+++ frontend/app/protected/tasks/page.tsx
@@ -1,3 +1,111 @@
-export default function TaskPage(){
-    return <>Task Page</>
+import { Task, columns } from "./columns"
+import { DataTable } from "./data-table"
+
+async function getData(): Promise<Task[]> {
+  // Fetch data from your API here.
+  return [
+    {
+      id: "728ed52f",
+      title: "OBM-100",
+      status: "backlog",
+      description: "Create ERD FutsalYuks!",
+    },
+    {
+      id: "728ed52123",
+      title: "OBM-100",
+      status: "backlog",
+      description: "Create ERD FutsalYuks!",
+    },
+    {
+      id: "12342134",
+      title: "OBM-100",
+      status: "backlog",
+      description: "Create ERD FutsalYuks!",
+    },
+    {
+      id: "12341234",
+      title: "OBM-100",
+      status: "backlog",
+      description: "Create ERD FutsalYuks!",
+    },
+    {
+      id: "728ed5123412342f",
+      title: "OBM-100",
+      status: "backlog",
+      description: "Create ERD FutsalYuks!",
+    },
+    // ...
+    {
+      id: "728ed52f",
+      title: "OBM-100",
+      status: "backlog",
+      description: "Create ERD FutsalYuks!",
+    },
+    {
+      id: "728ed52123",
+      title: "OBM-100",
+      status: "backlog",
+      description: "Create ERD FutsalYuks!",
+    },
+    {
+      id: "12342134",
+      title: "OBM-100",
+      status: "backlog",
+      description: "Create ERD FutsalYuks!",
+    },
+    {
+      id: "12341234",
+      title: "pop-100",
+      status: "backlog",
+      description: "Create ERD FutsalYuks!",
+    },
+    {
+      id: "728ed5123412342f",
+      title: "OBM-100",
+      status: "backlog",
+      description: "Create ERD FutsalYuks!",
+    },
+    // ...
+    {
+      id: "728ed52f",
+      title: "OBM-100",
+      status: "backlog",
+      description: "Create ERD FutsalYuks!",
+    },
+    {
+      id: "728ed52123",
+      title: "OBM-100",
+      status: "backlog",
+      description: "Create ERD FutsalYuks!",
+    },
+    {
+      id: "12342134",
+      title: "OBM-100",
+      status: "backlog",
+      description: "Create ERD FutsalYuks!",
+    },
+    {
+      id: "12341234",
+      title: "OBM-100",
+      status: "backlog",
+      description: "Create ERD FutsalYuks!",
+    },
+    {
+      id: "728ed5123412342f",
+      title: "OBM-100",
+      status: "backlog",
+      description: "Create ERD FutsalYuks!",
+    },
+    // ...
+  ]
+}
+
+export default async function TasksPage() {
+  const data = await getData()
+
+  return (
+    <div className="container mx-auto py-10">
+      <DataTable columns={columns} data={data} />
+    </div>
+  )
 }
