637b62154a9858850b1a3d9b290806e69ed9bfcb
setiawanDedi05 <dssetiawan05@gmail.com>
2024-09-02 21:43:15 +0700

    add use flobal pipes for enable validator request

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -2,9 +2,10 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as cookieParser from 'cookie-parser';
 import helmet from 'helmet';
+import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  
+  app.useGlobalPipes(new ValidationPipe())
   app.use(helmet());
 
   const allowedOrigins = [
