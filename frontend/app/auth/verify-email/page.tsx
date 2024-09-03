0ceb11e352a53dec33e3766cce39961fab2fa498
setiawanDedi05 <dssetiawan05@gmail.com>
2024-09-03 10:36:07 +0700

    fixing failed build

--- frontend/app/auth/verify-email/page.tsx
+++ frontend/app/auth/verify-email/page.tsx
@@ -9,7 +9,7 @@ import {
   CardHeader,
   CardTitle,
 } from "@/components/ui/card";
-import { authService } from "@/services/auth/authService";
+import { authService } from "@/services/auth/auth-service";
 import { useRouter, useSearchParams } from "next/navigation";
 import { useEffect, useState } from "react";
 
