89c0cdd37a609b72975eaa9a13e0a0865a79e70c
setiawanDedi05 <dssetiawan05@gmail.com>
2024-09-05 09:57:38 +0700

    restore middleware

---
+++ frontend/middleware.ts
@@ -0,0 +1,15 @@
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
+
+export function middleware(req: NextRequest) {
+  const token = req.cookies.get("token");
+  console.log({ token });
+  if (!token) {
+    return NextResponse.redirect(new URL("/", req.url));
+  }
+  return NextResponse.next();
+}
+
+export const config = {
+  matcher: ["/protected/:path*"],
+};
