7de85b58bdad4e620c817f038ad4e348cb3635ef
setiawanDedi05 <dssetiawan05@gmail.com>
2024-09-05 12:43:20 +0700

    update api service

--- frontend/services/api.ts
+++ frontend/services/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_API_URL || "http://103.196.153.135",
+  baseURL: `${process.env.NEXT_PUBLIC_API_URL}/api` || "http://localhost:3000",
   withCredentials: true,
 });
 
