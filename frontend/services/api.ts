854d9374022b9e8f3bf12066215a8be3546c9b66
Ubuntu <dedi05@bagman.neo.internal>
2024-09-08 11:51:40 +0700

    repairing enpoint and default api

--- frontend/services/api.ts
+++ frontend/services/api.ts
@@ -1,8 +1,10 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: `${process.env.NEXT_PUBLIC_API_URL}/api` || "http://localhost:3000",
-  withCredentials: true,
+  	headers: {
+		"Content-Type":"application/json"
+	},
+	withCredentials: true,
 });
 
 api.interceptors.response.use(
