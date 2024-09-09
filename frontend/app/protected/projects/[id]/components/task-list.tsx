ff4537b93655513baeac09996df647181ea8f496
setiawanDedi05 <dssetiawan05@gmail.com>
2024-09-09 22:21:02 +0700

    implement project detail, edit project, delete project and edit create project

---
+++ frontend/app/protected/projects/[id]/components/task-list.tsx
@@ -0,0 +1,88 @@
+import { Badge } from "@/components/ui/badge";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import {
+  PriorityTaskEnum,
+  Task,
+} from "@/services/dto/task-dto";
+import { formatDistanceToNow } from "date-fns/formatDistanceToNow";
+import { Search } from "lucide-react";
+import { useCallback, useEffect, useState } from "react";
+import parse from 'html-react-parser';
+
+interface TaskListProps {
+  tasks?: Array<Task>;
+}
+export default function TaskList({ tasks = [] }: TaskListProps) {
+  const [filteredTasks, setFilteredTasks] = useState<Task[]>([]);
+
+  useEffect(() => {
+    setFilteredTasks(tasks);
+  }, [tasks]);
+
+  const onSearch = useCallback(
+    (event: any) => {
+      const { value } = event.target;
+      console.log(value);
+      if (value && value.length > 3) {
+        setFilteredTasks(tasks.filter((task) => task.title.includes(value)));
+      } else {
+        setFilteredTasks(tasks);
+      }
+    },
+    [tasks, setFilteredTasks]
+  );
+
+  return (
+    <>
+      <div className="bg-background/95 p-4 backdrop-blur supports-[backdrop-filter]:bg-background/60">
+        <form>
+          <div className="relative">
+            <Search className="absolute left-2 top-2.5 h-4 w-4 text-muted-foreground" />
+            <Input placeholder="Search" className="pl-8" onKeyUp={onSearch} />
+          </div>
+        </form>
+      </div>
+      <div className="flex flex-col gap-2 p-4 pt-0">
+        {filteredTasks.map((item) => (
+          <Card>
+            <CardHeader>
+              <CardTitle className="flex justify-between align-center">
+                <span>{item.title}</span>
+                <span className="text-slate-500 text-sm">
+                  {formatDistanceToNow(
+                    new Date(item.createdAt! || new Date().toLocaleString()),
+                    {
+                      addSuffix: true,
+                    }
+                  )}
+                </span>
+              </CardTitle>
+              <CardDescription>
+                {item.label}
+                {" - "}
+                <Badge
+                  className="uppercase"
+                  variant={
+                    item.priority === PriorityTaskEnum.HIGH
+                      ? "destructive"
+                      : "secondary"
+                  }
+                >
+                  {item.priority}
+                </Badge>
+              </CardDescription>
+            </CardHeader>
+            <CardContent>{parse(item.description.substring(0, 300))}</CardContent>
+          </Card>
+        ))}
+      </div>
+    </>
+  );
+}
