f875713cd1ad66614ac7a8ba8508be4cdc476791
setiawanDedi05 <dssetiawan05@gmail.com>
2024-09-09 22:15:55 +0700

    remove console.log in middleware

--- frontend/middleware.ts
+++ frontend/middleware.ts
@@ -3,7 +3,6 @@ import type { NextRequest } from "next/server";
 
 export function middleware(req: NextRequest) {
   const token = req.cookies.get("token");
-  console.log({ token });
   if (!token) {
     return NextResponse.redirect(new URL("/", req.url));
   }
