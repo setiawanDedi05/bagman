644fa9bfef6dca0e02dda2e13a076439975af17a
setiawanDedi05 <dssetiawan05@gmail.com>
2024-09-10 17:35:28 +0700

    enhancement and integration search pagination, detail task, delete task, edit task

--- frontend/services/tasks/tasks-service.ts
+++ frontend/services/tasks/tasks-service.ts
@@ -1,12 +1,21 @@
 import { api } from "../api";
-import { CreateTaskRequest } from "../dto/task-dto";
+import { CreateTaskRequest, UpdateTaskRequest } from "../dto/task-dto";
 import { endpoint } from "../endpoints";
 
 export const tasksService = {
-  allTask: async () => {
-    return await api.get(endpoint.tasks.parent);
+  allTask: async (page: string) => {
+    return await api.get(`${endpoint.tasks.parent}?page=${page}`);
   },
   createTask: async (request: CreateTaskRequest) => {
     return await api.post(endpoint.tasks.parent, request);
+  },
+  detailTask: async (id: string) => {
+    return await api.get(`${endpoint.tasks.parent}/${id}`);
+  },
+  deleteTask: async (id: string) => {
+    return await api.delete(`${endpoint.tasks.parent}/${id}`);
+  },
+  updateTask: async (request: UpdateTaskRequest, id: string) => {
+    return await api.patch(`${endpoint.tasks.parent}/${id}`, request);
   },
 };
