f968e08b268e76513d1869ac9947a571ab66faf9
setiawanDedi05 <dssetiawan05@gmail.com>
2024-09-15 20:31:38 +0700

    repairing push notification

--- frontend/public/firebase-messaging-sw.js
+++ frontend/public/firebase-messaging-sw.js
@@ -1,21 +1,69 @@
-importScripts("https://www.gstatic.com/firebasejs/8.10.0/firebase-app.js");
+importScripts("https://www.gstatic.com/firebasejs/8.10.1/firebase-app.js");
 importScripts(
-  "https://www.gstatic.com/firebasejs/8.10.0/firebase-messaging.js"
+  "https://www.gstatic.com/firebasejs/8.10.1/firebase-messaging.js"
 );
 
-self.addEventListener("message", (event) => {
-  const { firebaseConfig } = event.data;
+// Replace these with your own Firebase config keys...
+const firebaseConfig = {
+  apiKey: "AIzaSyA6-XJFR475jESZA6gZKWvcFh4UToS1Rig",
+  authDomain: "bagman-05.firebaseapp.com",
+  projectId: "bagman-05",
+  storageBucket: "bagman-05.appspot.com",
+  messagingSenderId: "471179130423",
+  appId: "1:471179130423:web:792ba031c2e8bb289131b2",
+  measurementId: "G-KTDEPV1QPV"
+};
 
-  firebase.initializeApp(firebaseConfig);
-  const messaging = firebase.messaging();
 
-  messaging.onBackgroundMessage(function (payload) {
-    console.log("Received background message: ", payload);
-    const notificationTitle = payload.notification.title;
-    const notificationOptions = {
-      body: payload.notification.body,
-    };
+firebase.initializeApp(firebaseConfig);
 
-    self.registration.showNotification(notificationTitle, notificationOptions);
-  });
+const messaging = firebase.messaging();
+
+messaging.onBackgroundMessage((payload) => {
+  console.log(
+    "[firebase-messaging-sw.js] Received background message ",
+    payload
+  );
+
+  // payload.fcmOptions?.link comes from our backend API route handle
+  // payload.data.link comes from the Firebase Console where link is the 'key'
+  const link = payload.fcmOptions?.link || payload.data?.link;
+
+  const notificationTitle = payload.notification.title;
+  const notificationOptions = {
+    body: payload.notification.body,
+    icon: "./logo.png",
+    data: { url: link },
+  };
+  self.registration.showNotification(notificationTitle, notificationOptions);
+});
+
+self.addEventListener("notificationclick", function (event) {
+  console.log("[firebase-messaging-sw.js] Notification click received.");
+
+  event.notification.close();
+
+  // This checks if the client is already open and if it is, it focuses on the tab. If it is not open, it opens a new tab with the URL passed in the notification payload
+  event.waitUntil(
+    clients
+      // https://developer.mozilla.org/en-US/docs/Web/API/Clients/matchAll
+      .matchAll({ type: "window", includeUncontrolled: true })
+      .then(function (clientList) {
+        const url = event.notification.data.url;
+
+        if (!url) return;
+
+        // If relative URL is passed in firebase console or API route handler, it may open a new window as the client.url is the full URL i.e. https://example.com/ and the url is /about whereas if we passed in the full URL, it will focus on the existing tab i.e. https://example.com/about
+        for (const client of clientList) {
+          if (client.url === url && "focus" in client) {
+            return client.focus();
+          }
+        }
+
+        if (clients.openWindow) {
+          console.log("OPENWINDOW ON CLIENT");
+          return clients.openWindow(url);
+        }
+      })
+  );
 });
