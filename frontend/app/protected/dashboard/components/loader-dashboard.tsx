3cb19bf47b1c128eaedf644bc106a2c7639b132e
setiawanDedi05 <dssetiawan05@gmail.com>
2024-09-18 21:53:08 +0700

    fixing error build

--- frontend/app/protected/dashboard/components/loader-dashboard.tsx
+++ frontend/app/protected/dashboard/components/loader-dashboard.tsx
@@ -5,10 +5,10 @@ export default function LoaderDashboard() {
     <div className="flex flex-col gap-5">
       {Array.from([1, 2, 3, 4, 5]).map((_, index) => {
         return (
-          <div className="w-full grid gap-4 grid-cols-1 md:grid-cols-2">
+          <div key={index} className="w-full grid gap-4 grid-cols-1 md:grid-cols-2">
             <Skeleton className="h-[125px] w-[250px] rounded-xl" />
             <div className="flex flex-col space-y-3">
-              <div key={index} className="space-y-2">
+              <div className="space-y-2">
                 <Skeleton className="h-4 w-[250px]" />
                 <Skeleton className="h-4 w-[200px]" />
               </div>
